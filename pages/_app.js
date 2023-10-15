07c2d23a53d4c7022078b3ab746e101b25e753d8
vigneshwaran <vigneshwaran.kannan@scripbox.com>
2023-10-15 19:17:20 +0530

    feat: modify tailwind css

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import 'tailwindcss/tailwind.css';
+import '../src/styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,20 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    theme: {
+      screens: {
+        mobile: '320px',
+        // => @media (min-width: 640px) { ... }
+        tablet: '640px',
+        // => @media (min-width: 640px) { ... }
+
+        laptop: '1024px',
+        // => @media (min-width: 1024px) { ... }
+
+        desktop: '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
+    },
     extend: {
       fontFamily: {
         primary: ['Inter', ...defaultTheme.fontFamily.sans],
