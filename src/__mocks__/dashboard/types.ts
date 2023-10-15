7d8187e9251dd73cbd0a737721c9ed15212f23b6
vigneshwaran <vigneshwaran.kannan@scripbox.com>
2023-10-15 18:02:53 +0530

    feat: add mock data and its type definition

---
+++ src/__mocks__/dashboard/types.ts
@@ -0,0 +1,61 @@
+export interface Root {
+  kind: string;
+  etag: string;
+  nextPageToken: string;
+  regionCode: string;
+  pageInfo: PageInfo;
+  items: DashboardItem[];
+}
+
+export interface PageInfo {
+  totalResults: number;
+  resultsPerPage: number;
+}
+
+export interface DashboardItem {
+  kind: string;
+  etag: string;
+  id: Id;
+  snippet: Snippet;
+}
+
+export interface Id {
+  kind: string;
+  videoId?: string;
+  channelId?: string;
+}
+
+export interface Snippet {
+  publishedAt: string;
+  channelId: string;
+  title: string;
+  description: string;
+  thumbnails: Thumbnails;
+  channelTitle: string;
+  liveBroadcastContent: string;
+  publishTime: string;
+}
+
+export interface Thumbnails {
+  default: Default;
+  medium: Medium;
+  high: High;
+}
+
+export interface Default {
+  url: string;
+  width?: number;
+  height?: number;
+}
+
+export interface Medium {
+  url: string;
+  width?: number;
+  height?: number;
+}
+
+export interface High {
+  url: string;
+  width?: number;
+  height?: number;
+}
